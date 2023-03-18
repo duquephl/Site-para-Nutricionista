9d1466736922024bb63baba858934f4af8058618
Judah <ueldjudah@gmail.com>
2023-03-18 15:53:02 -0300

    criando model e adm

--- Contato/models.py
+++ Contato/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Contato(models.Model):
+    nome = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    assunto = models.CharField(max_length=100)
+    mensagem = models.TextField()
+
+    def __str__(self):
+        return self.nome
