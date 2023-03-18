5d434fffacb77858fa1dfc9606f92c2a2527790e
Pedro Duque <euphlduque@gmail.com>
2023-03-18 15:46:47 -0300

    :art:  User 1.3 criando models.py

--- User/models.py
+++ User/models.py
@@ -1,17 +1,67 @@
+from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
+from datetime import timezone
+
 
 # Create your models here.
 
+class UserManager(BaseUserManager):
+    def _create_user(self, username, email, password, is_staff, is_superuser, **extra_fields):
+        if not username:
+            raise ValueError('The given username must be set')
+        email = self.normalize_email(email)
+        user = self.model(username=username, email=email, is_staff=is_staff, is_active=True, is_superuser=is_superuser,
+                          **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
-class User(models.Model):
-    name = models.CharField(max_length=50)
-    email = models.EmailField(max_length=50)
-    password = models.CharField(max_length=50)
-    phone = models.IntegerField()
-    data_joined = models.DateTimeField(auto_now_add=True)
-    data_de_nascimento = models.DateField()
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        return self._create_user(username, email, password, False, False, **extra_fields)
 
+    def create_superuser(self, username, email, password, **extra_fields):
+        user = self._create_user(username, email, password, True, True, **extra_fields)
+        user.is_active = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(max_length=50, unique=True)
+    email = models.EmailField(max_length=50, unique=True)
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+    last_login = models.DateTimeField(auto_now=True)
+    data_de_nascimento = models.DateField(null=True, blank=True)
+    cpf = models.CharField(max_length=11)
+    telefone = models.CharField(max_length=11)
+    endereco = models.CharField(max_length=100)
+    cidade = models.CharField(max_length=50)
+    estado = models.CharField(max_length=2)
+    cep = models.CharField(max_length=8)
+
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    objects = UserManager()
 
     def __str__(self):
-        return self.name
+        return self.username
 
+    def get_full_name(self):
+        return self.username
+
+    def get_short_name(self):
+        return self.username
+
+    def email_user(self, subject, message, from_email=None, **kwargs):
+        pass
+
+    class Meta:
+        db_table = 'User'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+        ordering = ['id']
