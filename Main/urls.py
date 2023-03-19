e9bec480eefcc72999e488cdfde94b500819eb6d
Pedro Duque <euphlduque@gmail.com>
2023-03-19 02:52:44 -0300

    :art: mexendo nos html

--- Main/urls.py
+++ Main/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import enviar_mensagem
+from .views import SobreView
 
 from Main.views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('enviar-mensagem/', enviar_mensagem, name='enviar_mensagem'),
+    path('sobre/', SobreView.as_view(), name='sobre'),
 ]
--- Main/views.py
+++ Main/views.py
@@ -3,22 +3,12 @@ from django.views.generic import TemplateView
 from .models import Mensagem
 from django.http import HttpResponse
 
-def enviar_mensagem(request):
-    if request.method == 'POST':
-        nome = request.POST['nome']
-        email = request.POST['email']
-        assunto = request.POST['assunto']
-        mensagem = request.POST['mensagem']
-        
-        nova_mensagem = Mensagem(nome=nome, email=email, mensagem=mensagem)
-        nova_mensagem.save()
-        
-        return HttpResponse('Mensagem enviada com sucesso!')
-    
-    return render(request, 'seu_template.html')
 
 # Create your views here.
 
 class HomeView(TemplateView):
     template_name = 'Main/home.html'
 
+
+class SobreView(TemplateView):
+    template_name = 'Main/sobre.html'
